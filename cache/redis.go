292f57f4507d9c0050c6747676ed68dfd5e8865d
Prakash Suresh <kongumechatron@gmail.com>
2024-12-14 11:01:33 +0530

    Removed Redis token data from redis.go

--- cache/redis.go
+++ cache/redis.go
@@ -12,14 +12,6 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-// RedisTokenData represents token information stored in Redis
-type RedisTokenData struct {
-	AccessToken string `json:"access_token"`
-	CreatedAt   int64  `json:"created_at"` // Unix milliseconds
-	ExpiresAt   int64  `json:"expires_at"` // Unix milliseconds
-	IsValid     bool   `json:"is_valid"`
-}
-
 // Constants for database numbers
 const (
 	IndexSpotDB     = 1 // For index spot data
